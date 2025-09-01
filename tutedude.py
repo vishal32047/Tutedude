3dea7efdc51f5e25bb1a603843b770b175266126
Vishal Kumar <77271410+vishal32047@users.noreply.github.com>
2025-09-01 12:16:19 +0530

    Add files via upload

---
+++ tutedude.py
@@ -0,0 +1,24 @@
+# First task
+
+firstdigit = int(input('enter 1st digit'))
+seconddigit = int(input('enter 2nd digit'))
+
+addition = firstdigit + seconddigit
+subtraction = firstdigit - seconddigit
+multiplication = firstdigit * seconddigit
+division = firstdigit // seconddigit
+
+print(f"Addition = {addition}")
+print(f"Subtraction = {subtraction}")
+print(f"Multiplication = {multiplication}")
+print(f"Division = {division}")
+
+
+# Second Task
+
+firstName = input('Enter first name: ')
+lastName = input('Enter last name: ')
+
+fullName = firstName + " " + lastName
+
+print('welcome, '+fullName+'! to the python course')
